40e1f0710f19fbd693914de4665543fa00697e18
bunabloom <kimrindev@gmail.com>
2024-06-28 14:14:11 +0900

    Initial Commit

---
+++ Cal_Codebase/View/SubView.swift
@@ -0,0 +1,24 @@
+//
+//  SubView.swift
+//  Cal_Codebase
+//
+//  Created by rin kim on 6/28/24.
+//
+
+import UIKit
+import SnapKit
+class SubView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been imolemented")
+    }
+
+
+
+}
---
+++ Cal_Codebase/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Cal_Codebase
+//
+//  Created by rin kim on 6/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var subView = SubView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+        
+    }
+
+
+}
+
+
+#Preview{
+    let vc = ViewController()
+    return vc
+}
+
